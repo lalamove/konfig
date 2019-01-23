f32b932d93006cd9402eca43b869a40cf5493701
francoispqt <francois@parquet.ninja>
2019-01-23 13:37:28 +0800

    add tests

--- parser/kpmap/mapparser_test.go
+++ parser/kpmap/mapparser_test.go
@@ -1,1 +1,39 @@
 package kpmap
+
+import (
+	"testing"
+
+	"github.com/lalamove/konfig"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapPopFlatten(t *testing.T) {
+	var m = map[string]interface{}{
+		"test": map[string]interface{}{
+			"foo": "bar",
+		},
+		"testIface": map[interface{}]interface{}{
+			1: "bar",
+			"testIface": map[interface{}]interface{}{
+				"foo": "bar",
+			},
+			"test": map[string]interface{}{
+				"foo": "bar",
+			},
+		},
+	}
+
+	var v = konfig.Values{}
+	PopFlatten(m, v)
+
+	require.Equal(
+		t,
+		konfig.Values{
+			"test.foo":                "bar",
+			"testIface.1":             "bar",
+			"testIface.testIface.foo": "bar",
+			"testIface.test.foo":      "bar",
+		},
+		v,
+	)
+}
